376c25994c13b20a55b8f4b0fe56097b926a82d9
svitlana-sl <svitlana.slobodeniuk@gmail.com>
2022-11-19 20:32:34 +0100

    Showing the current temperature

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
+function displayTemparature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "e9701c7dc6b2af755127baee6b97c965";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemparature);
