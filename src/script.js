c223a22d50b1c3dba4f3f058fba03c2620828bdc
svitlana-sl <svitlana.slobodeniuk@gmail.com>
2022-11-20 17:11:01 +0100

    fixed celsiusTemperature bug, changed btn style

--- src/script.js
+++ src/script.js
@@ -30,7 +30,10 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
